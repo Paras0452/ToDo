5772315a6abee707af81be4fe50af56da71a558b
Paras Dhumne <parasdhumne16@gmail.com>
2024-10-05 10:38:00 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const {createTodo, updateTodo} = require("./types")
 const app = express()
 
 app.use(express.json())
@@ -8,9 +9,25 @@ app.get("/todos",function(req,res){
 })
 
 app.put("/completed", function(req,res){
-
+const updatePayload = req.body;
+const parsedPayload = updateTodo.safeParse(updatePayload);
+if(!parsedPayload.success){
+    res.status(411).json({
+        msg : "Ypu sent the wrong inputs",
+    })
+    return
+}
+ 
 })
 
 app.post("/todo",function(req,res){
-    
+const createPayload = req.body;
+const parsedPayload = createTodo.safeParse(createPayload);
+if(!parsedPayload.success){
+    res.status(411).json({
+        msg : "You sent the wrong inputs",
+    })
+    return
+}
+
 })
