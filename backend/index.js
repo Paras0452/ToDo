dca2498740af2ff5967a7e0b2a6894e1b94dcc29
Paras Dhumne <parasdhumne16@gmail.com>
2024-10-06 13:22:39 +0530

    completed backend & initialised react for frontend

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,54 @@
 const express = require("express")
-const {createTodo, updateTodo} = require("./types")
-const {todo} = require("./db.js")
+const { createTodo, updateTodo } = require("./types")
+const { todo } = require("./db.js")
 const app = express()
 
 app.use(express.json())
 
-app.get("/todos",function(req,res){
-
-})
-
-app.put("/completed", function(req,res){
-const updatePayload = req.body;
-const parsedPayload = updateTodo.safeParse(updatePayload);
-if(!parsedPayload.success){
-    res.status(411).json({
-        msg : "Ypu sent the wrong inputs",
+app.get("/todos", async function (req, res) {
+    const todos = await todo.find({});
+    res.json({
+        todos
     })
-    return
-}
-
-
 })
 
-app.post("/todo",async function(req,res){
-const createPayload = req.body;
-const parsedPayload = createTodo.safeParse(createPayload);
-if(!parsedPayload.success){
-    res.status(411).json({
-        msg : "You sent the wrong inputs",
+app.put("/completed", async function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return
+    }
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
     })
-    return
-}
-await todo.create({
-    title : createPayload.title,
-    description : createPayload.description,
+    res.json({
+        msg: "ToDo marked as completed"
+    })
 
 })
-res.json({
-    msg : "ToDo created"
+
+app.post("/todo", async function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return
+    }
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "ToDo created"
+    })
 })
-})
+
+app.listen(3000);
