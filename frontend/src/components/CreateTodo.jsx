8fae258475bed185ba2505a7dfaa8dc92483171d
Paras Dhumne <parasdhumne16@gmail.com>
2024-10-06 14:27:19 +0530

    Created component

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo(){
+    return (
+        <div>
+            <input type="text" placeholder="Title" /> <br></br>
+            <input type="text" placeholder="Description" /> <br />
+
+            <button>Add a ToDo</button>
+        </div>
+    )
+}
